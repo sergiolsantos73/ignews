7f5450d37022fa4a6a1cb851cd93776671eefe61
Sergio <sergioluiz73@gmail.com>
2022-06-17 17:13:54 -0300

    Procurando erro auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -74,7 +74,8 @@ export default NextAuth({
           )
         );
         return true;
-      } catch {
+      } catch (error){
+        console.log(error);
         return false;
       }
     },
