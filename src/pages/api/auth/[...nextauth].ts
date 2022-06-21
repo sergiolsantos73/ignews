bd7d7fd4e3ca4a0486c87c3488c6946c66235932
Sergio <sergioluiz73@gmail.com>
2022-06-21 19:03:38 -0300

    teste signin

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -57,27 +57,27 @@ export default NextAuth({
     },
     async signIn({ user, account, profile }) {
       const { email } = user;
-      try {
-        await fauna.query(
-          q.If(
-            q.Not(
-              q.Exists(
-                q.Match(q.Index("user_by_email"), 
-                q.Casefold(user.email))
-              )
-            ),
-            q.Create(q.Collection("users"), { data: { email } }),
-            q.Get(
-              q.Match(q.Index("user_by_email"), 
-              q.Casefold(user.email))
-            )
-          )
-        );
-        return true;
-      } catch (error){
-        console.log(error);
-        return false;
-      }
-    },
-  },
+      // try {
+      //   await fauna.query(
+      //     q.If(
+      //       q.Not(
+      //         q.Exists(
+      //           q.Match(q.Index("user_by_email"), 
+      //           q.Casefold(user.email))
+      //         )
+      //       ),
+      //       q.Create(q.Collection("users"), { data: { email } }),
+      //       q.Get(
+      //         q.Match(q.Index("user_by_email"), 
+      //         q.Casefold(user.email))
+      //       )
+      //     )
+      //   );
+      //   return true;
+      // } catch (error){
+      //   console.log(error);
+      //   return false;
+      // }
+    }
+  }
 });
